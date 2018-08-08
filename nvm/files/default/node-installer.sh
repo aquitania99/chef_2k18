df01c544ebd64623076583c30b53c3e3c2fa995b
Sergio Medina <sergiomedina@veromo.com.au>
2018-08-08 17:01:14 +1000

    NVM - WIP

--- nvm/files/default/node-installer.sh
+++ nvm/files/default/node-installer.sh
@@ -7,10 +7,11 @@ wget https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh
 echo "LIST FOLDER CONTENT: `ls -ltrha`"
 mkdir /home/ubuntu/.nvm
 echo "INSTALL NVM"
-cp install.sh /home/ubuntu
+mv /tmp/install.sh /home/ubuntu
 cd /home/ubuntu
-chown 755 install.sh
 echo "PWD: `pwd`"
+echo "755 to install.sh"
+sudo chown 755 install.sh
 ./install.sh
 # echo "CHANGE DIR"
 # sudo chown ubuntu:ubuntu -R /home/ubuntu/.nvm
