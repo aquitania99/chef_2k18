1266f2cddb4a2d7653cd7b6efa332071f3f21dc9
Sergio Medina <sergiomedina@veromo.com.au>
2018-08-21 23:48:28 +1000

    Bitbucket Clone Repo - WIP

--- nodejs/recipes/deploy.rb
+++ nodejs/recipes/deploy.rb
@@ -8,16 +8,24 @@
 #
 Chef::Log.info("******* DEPLOYING NODEJS APP *******")
 
-bash "git clone git@bitbucket.org:5andhalf/barbooks-nodejs.git" do
-    cwd '/home/ubuntu'
-    user 'ubuntu'
-    group 'ubuntu'
-    environment ({'HOME' => '/home/ubuntu', 'USER' => 'ubuntu'})
-    code <<-EOH
-			echo "PWD: `pwd`"
-      git clone git@bitbucket.org:5andhalf/barbooks-nodejs.git
-      cd barbooks-nodejs
-      git checkout development
-      npm install
-      EOH
+# bash "git clone" do
+#     cwd '/home/ubuntu'
+#     user 'ubuntu'
+#     group 'ubuntu'
+#     environment ({'HOME' => '/home/ubuntu', 'USER' => 'ubuntu'})
+#     code <<-EOH
+# 			echo "PWD: `pwd`"
+#       git clone git@bitbucket.org:5andhalf/barbooks-nodejs.git
+#       cd barbooks-nodejs
+#       git checkout development
+#       npm install
+#       EOH
+# end
+
+deploy 'private_repo' do
+  repo 'git@bitbucket.org:5andhalf/barbooks-nodejs.git'
+  user 'ubuntu'
+  deploy_to '/home/ubuntu/barbooks-nodejs'
+  ssh_wrapper '/tmp/git_ssh_wrapper.sh'
+  action :deploy
 end
